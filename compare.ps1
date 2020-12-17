06f35a1326465ba8acf16284fa46d00165a156a3
Jake Levitt <jlevitt@vt.edu>
2020-12-17 14:19:05 -0500

    Update for mod groups.

--- compare.ps1
+++ compare.ps1
@@ -33,6 +33,16 @@ function Filter-MenuItems($json)
     $json | jq "._embedded.menu_items | sort_by(.id) | .[] | del(._embedded.option_sets) | del(._embedded.price_levels[].barcodes) | del(.barcodes)"
 }
 
+function Filter-MenuModifiers($json)
+{
+    $json | jq "._embedded.modifiers | sort_by(.id) | .[] | del(._embedded.option_sets) | del(._embedded.price_levels[].barcodes) | del(.barcodes)"
+}
+
+function Filter-MenuModifierGroups($json)
+{
+    $json | jq -f modifier-groups.jq
+}
+
 function run($resource)
 {
     mysql -h 127.0.0.1 -u root -P 3310 --password=$MYSQL_PASSWORD -e "update agent_master.store_scheduled_tasks set next_run=now(), start_time=null where store_id=5 and name = 'menu';"
@@ -49,8 +59,26 @@ function run($resource)
     $aPath = "diffs\$($resource.Replace("/", "_")).a.json"
     $bPath = "diffs\$($resource.Replace("/", "_")).b.json"
 
-    Filter-MenuItems($(pyapi "/$resource")) > $aPath
-    Filter-MenuItems($(goapi "/$resource")) > $bPath
+    if ($resource -eq "menu/modifiers")
+    {
+        Filter-MenuModifiers $(pyapi "/$resource") > $aPath
+        Filter-MenuModifiers $(goapi "/$resource") > $bPath
+    }
+    elseif ($resource -eq "menu/items")
+    {
+        Filter-MenuItems $(pyapi "/$resource") > $aPath
+        Filter-MenuItems $(goapi "/$resource") > $bPath
+    }
+    elseif ($resource -eq "menu/modifier_groups")
+    {
+        Filter-MenuModifierGroups $(pyapi "/$resource") > $aPath
+        Filter-MenuModifierGroups $(goapi "/$resource") > $bPath
+    }
+    else
+    {
+        $(pyapi "/$resource") > $aPath
+        $(goapi "/$resource") > $bPath
+    }
     kdiff3 $aPath $bPath
 }
 
